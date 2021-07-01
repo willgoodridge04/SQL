772d86852cefef3efc31614382d0d9a39473cb37
willgoodridge04 <66393809+willgoodridge04@users.noreply.github.com>
2021-07-01 16:52:19 -0500

    Update Dim_Customer_v2.sql

--- Dim_Customer_v2.sql
+++ Dim_Customer_v2.sql
@@ -36,3 +36,32 @@ FROM
   Left Join dbo.DimGeography as g on g.GeographyKey = c.GeographyKey 
 order by 
   CustomerKey ASC  -- ordered list by acsending CustomerKey
+
+---------------------------------------------------------------------------------------------
+
+  
+-- Cleansed DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] as Date, 
+  --[DayNumberOfWeek],
+  [EnglishDayNameOfWeek] as Day, 
+  --[SpanishDayNameOfWeek]
+  --[FrenchDayNameOfWeek]
+  --[DayNumberOfMonth]
+  --[DayNumberOfYear]
+  --[WeekNumberOfYear]
+  [EnglishMonthName] as Month, 
+  Left([EnglishMonthName], 3) as MonthShort, 
+  --[SpanishMonthName]
+  --[FrenchMonthName]
+  [MonthNumberOfYear] as MonthNo, 
+  [CalendarQuarter] as Quarter, 
+  [CalendarYear] as Year --[CalendarSemester]
+  --[FiscalQuarter]
+  --[FiscalYear]
+  --[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+where 
+  CalendarYear >= 2019
